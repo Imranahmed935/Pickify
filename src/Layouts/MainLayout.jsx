69312539aad00b75090cf33938226d42311c2cbf
Imranahmed935 <imrantahir9918@gmail.com>
2024-12-21 11:38:04 -0800

    project setup and a eye-catching navbar added

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "../Components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer/Footer";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="w-9/12 mx-auto">
+      <Outlet />
+      </div>
+
+      {/* <Footer/> */}
+    </div>
+  );
+};
+
+export default MainLayout;
